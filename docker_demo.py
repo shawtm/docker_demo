bf701b61ff219f26b10235b7d490a5be8a7f6e39
shawtm <shawtm@rams.colostate.edu>
2019-07-29 13:55:51 -0600

    add redis to stack

--- docker_demo.py
+++ docker_demo.py
@@ -1,13 +1,21 @@
 from flask import Flask, jsonify
+from redis import RedisError, Redis
 import socket
 
 
 app = Flask(__name__)
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 
 @app.route('/')
 def hello_ram():
-    return jsonify("Hello Ram! I'm {}".format(socket.gethostname()))
+    i_am = socket.gethostname()
+    try:
+        visits = redis.incr(i_am)
+    except RedisError:
+        visits = 0
+    return jsonify("Hi Ram, I'm {}. I've been seen {} times".format(
+        i_am, visits))
 
 
 app.run(host="0.0.0.0", port=5000)
